7323aca7daf32de4be5eb65a28bb9728e5000e3d
Martin Sosa <martinleonelarce@gmail.com>
2020-11-21 16:56:55 +0000

    Update tp.go add funcion creartablas

--- tp.go
+++ tp.go
@@ -41,6 +41,28 @@ func CreateDatabase(db *sql.DB) {
 	}
 }
 
+func CrearTablar(db *sql.DB) {
+
+	_, err = db.Exec(`	create table cliente (nrocliente int, nombre text, apellido text, domicilio text, telefono varhar(12))
+						create table tarjeta (nrotarjeta varchar(16), nrocliente int, validadesde varchar(6), validahasta varchar(6),codseguridad varchar(4), limitecompra decimal(8,2), estado varchar(10))
+						
+						create table comercio (nrocomercio int, nombre text, domicilio text, codigopostal varchar(8), telefono varchar(12))
+						create table compra (nrooperacion int, nrotarjeta varchar(16), nrocomercio int, fecha timestamp, monto decimal(7,2), pagado boolean)
+
+						create table rechazo (nrorechazo int, nrotarjeta varchar(16), nrocomercio int, fecha timestamp, monto decimal(7,2), motivo text)
+
+						create table cierre (anio int, mes int, terminacion int, fechainicio date, fechacierre date, fechavto date)
+						create table cabecera (nroresumen int, nombre text, apellido text, domicilio text, nrotarjeta varchar(16), desde date, hasta date, vence date, total decimal(8,2))
+
+						create table detalle (nroresumen int, nrolinea int, fecha date, nombrecomercio text, monto decimal(7,2))
+
+						create table alerta (nroalerta int, nrotarjeta varchar(16), fecha timestamp, nrorechazo int, codalerta int, descripcion text)
+
+						create table consumo (nrotarjeta varchar(16), codseguridad varchar(4), nrocomercio int, monto decimal(7,2))`)
+    if err != nil {
+        log.Fatal(err)
+    }
+
 func Bienvenida(db *sql.DB) {
 	menu :=
 		`
